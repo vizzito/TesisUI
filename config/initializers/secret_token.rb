dd3706ff7da4e339d27294124141fb7af88e4215
Martin Vizzolini <martinvizzolini@gmail.com>
2014-04-09 02:46:22 -0300

    config changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TesisUI::Application.config.secret_key_base = '85fd0f07d0e151c78127f8d6609853524a16f514af0035a44a639954bb4f778b998a4e315777aee5bc262ff6c9061d4f04b1e0d20b3c42cc59d642310adcaca2'
